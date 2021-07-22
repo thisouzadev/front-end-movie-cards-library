26e208334cfffddea06bddb28baeae2e3fde454c
Thiago <thiagodesouza.dev@gmail.com>
2021-07-22 16:37:27 -0300

    requisitos 11 ao 14 Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,18 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export class MovieCard extends Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div>
+      <div className="movie-card">
         <h4 className="page-title">{ title }</h4>
         <img src={ imagePath } alt={ title } />
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+        <h5 className="page-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
 // implement Rating component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Rating extends Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="rating">
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
